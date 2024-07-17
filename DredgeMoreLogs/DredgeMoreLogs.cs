91d8fb84f200b523bbc9bd73d529471f6223ec3e
Noah Pilarski <megapiggy@outerwildsmods.com>
2024-07-17 13:16:58 -0400

    Remove [UnityEngine.Debug.Log]

--- DredgeMoreLogs/DredgeMoreLogs.cs
+++ DredgeMoreLogs/DredgeMoreLogs.cs
@@ -10,35 +10,35 @@ public static class DredgeMoreLogs
 {
 	[HarmonyPostfix]
 	[HarmonyPatch(typeof(Debug), nameof(Debug.Log), new Type[] { typeof(object) })]
-	public static void Debug_Log(object message) => WinchCore.Log.Info($"[UnityEngine.Debug.Log] {message}");
+	public static void Debug_Log(object message) => WinchCore.Log.Info(message);
 
 	[HarmonyPostfix]
 	[HarmonyPatch(typeof(Debug), nameof(Debug.Log), new Type[] { typeof(object), typeof(UnityEngine.Object) })]
-	public static void Debug_Log(object message, UnityEngine.Object context) => WinchCore.Log.Info($"[UnityEngine.Debug.Log] {message}");
+	public static void Debug_Log(object message, UnityEngine.Object context) => WinchCore.Log.Info(message);
 
 	[HarmonyPostfix]
 	[HarmonyPatch(typeof(Debug), nameof(Debug.LogWarning), new Type[] { typeof(object) })]
-	public static void Debug_LogWarning(object message) => WinchCore.Log.Warn($"[UnityEngine.Debug.Log] {message}");
+	public static void Debug_LogWarning(object message) => WinchCore.Log.Warn(message);
 
 	[HarmonyPostfix]
 	[HarmonyPatch(typeof(Debug), nameof(Debug.LogWarning), new Type[] { typeof(object), typeof(UnityEngine.Object) })]
-	public static void Debug_LogWarning(object message, UnityEngine.Object context) => WinchCore.Log.Warn($"[UnityEngine.Debug.Log] {message}");
+	public static void Debug_LogWarning(object message, UnityEngine.Object context) => WinchCore.Log.Warn(message);
 
 	[HarmonyPostfix]
 	[HarmonyPatch(typeof(Debug), nameof(Debug.LogError), new Type[] { typeof(object) })]
-	public static void Debug_LogError(object message) => WinchCore.Log.Error($"[UnityEngine.Debug.Log] {message}");
+	public static void Debug_LogError(object message) => WinchCore.Log.Error(message);
 
 	[HarmonyPostfix]
 	[HarmonyPatch(typeof(Debug), nameof(Debug.LogError), new Type[] { typeof(object), typeof (UnityEngine.Object) })]
-	public static void Debug_LogError(object message, UnityEngine.Object context) => WinchCore.Log.Error($"[UnityEngine.Debug.Log] {message}");
+	public static void Debug_LogError(object message, UnityEngine.Object context) => WinchCore.Log.Error(message);
 
 	[HarmonyPostfix]
 	[HarmonyPatch(typeof(Debug), nameof(Debug.LogException), new Type[] { typeof(Exception) })]
-	public static void Debug_LogException(Exception exception) => WinchCore.Log.Error($"[UnityEngine.Debug.Log] {exception}");
+	public static void Debug_LogException(Exception exception) => WinchCore.Log.Error(exception);
 
 	[HarmonyPostfix]
 	[HarmonyPatch(typeof(Debug), nameof(Debug.LogException), new Type[] { typeof(Exception), typeof(UnityEngine.Object) })]
-	public static void Debug_LogException(Exception exception, UnityEngine.Object context) => WinchCore.Log.Error($"[UnityEngine.Debug.Log] {exception}");
+	public static void Debug_LogException(Exception exception, UnityEngine.Object context) => WinchCore.Log.Error(exception);
 
 	[HarmonyPrefix]
 	[HarmonyPatch(typeof(ItemManager), nameof(ItemManager.CreateFishItem))]
